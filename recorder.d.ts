2fa6a6937aad8de7215689113bab0d76dc3999a7
Benz <benz@youme.im>
2018-07-13 12:15:50 +0800

    哈哈哈哈

---
+++ recorder.d.ts
@@ -0,0 +1,11 @@
+class Recorder {
+    constructor();
+    public init(stream: MediaStream): Promise<void>;
+    public record(): void;
+    public stop(): void;
+    public clear(): void;
+    public getBuffer(cb: Function): void;
+    public exportWAV(cb: Function, type?: string): void;
+    public static forceDownload(blob: Blob, filename: string): void;
+}
+export default Recorder;
