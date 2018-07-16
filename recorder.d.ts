5f5e4a5e26ea5ddecb80bacd847168b2a25ffafe
MIO <mio@youme.im>
2018-07-16 14:12:45 +0800

    补充constructor参数

--- recorder.d.ts
+++ recorder.d.ts
@@ -1,11 +1,10 @@
 class Recorder {
     constructor(source: AudioBufferSourceNode, config?: {
-        workerPath: 'js/recorderjs/recorderWorker.js',
-        bufferLen: 4096,
-        callback: Function,
-        type: 'audio/wav'
+        workerPath?: string,
+        bufferLen?: number,
+        callback?: Function,
+        type?: string
     });
-    public init(stream: MediaStream): Promise<void>;
     public record(): void;
     public stop(): void;
     public clear(): void;
